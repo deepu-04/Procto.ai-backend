58b6d8e56302a7685ac4be666405a3c26bb099be
deepu-04 <22pa1a12e7@vishnu.edu.in>
2026-03-08 13:45:03 +0530

    initial commit

--- controllers/resultController.js
+++ controllers/resultController.js
@@ -1,12 +1,10 @@
 import asyncHandler from "express-async-handler";
 import Result from "../models/resultModel.js";
-import Exam from "../models/examModel.js";
 
 /*
 SAVE RESULT
 */
 const saveResult = asyncHandler(async (req, res) => {
-
   const { examId, answers, totalMarks, percentage } = req.body;
 
   if (!examId) {
@@ -25,72 +23,48 @@ const saveResult = asyncHandler(async (req, res) => {
   res.status(201).json(result);
 });
 
-
 /*
 GET USER RESULTS
 */
 const getUserResults = asyncHandler(async (req, res) => {
-
   const results = await Result.find({
     userId: req.user._id,
     showToStudent: true,
-  }).populate("userId", "name email");
+  })
+    .populate("userId", "name email")
+    .populate("examId");
 
-  const exams = await Exam.find();
-
-  const merged = results.map((r) => ({
-    ...r.toObject(),
-    examId: exams.find((e) => e.examId === r.examId),
-  }));
-
-  res.json(merged);
+  res.json(results);
 });
-
 
 /*
 GET ALL RESULTS (TEACHER)
 */
 const getAllResults = asyncHandler(async (req, res) => {
-
   const results = await Result.find()
-    .populate("userId", "name email");
+    .populate("userId", "name email")
+    .populate("examId");
 
-  const exams = await Exam.find();
-
-  const merged = results.map((r) => ({
-    ...r.toObject(),
-    examId: exams.find((e) => e.examId === r.examId),
-  }));
-
-  res.json(merged);
+  res.json(results);
 });
-
 
 /*
 GET RESULTS BY EXAM
 */
 const getResultsByExam = asyncHandler(async (req, res) => {
-
   const results = await Result.find({
     examId: req.params.examId,
-  }).populate("userId", "name email");
+  })
+    .populate("userId", "name email")
+    .populate("examId");
 
-  const exams = await Exam.find();
-
-  const merged = results.map((r) => ({
-    ...r.toObject(),
-    examId: exams.find((e) => e.examId === r.examId),
-  }));
-
-  res.json(merged);
+  res.json(results);
 });
-
 
 /*
 TOGGLE VISIBILITY
 */
 const toggleVisibility = asyncHandler(async (req, res) => {
-
   const result = await Result.findById(req.params.resultId);
 
   if (!result) {
@@ -102,8 +76,10 @@ const toggleVisibility = asyncHandler(async (req, res) => {
 
   await result.save();
 
-  res.json({ message: "Visibility updated successfully" });
-
+  res.json({
+    message: "Visibility updated successfully",
+    showToStudent: result.showToStudent,
+  });
 });
 
 export {
